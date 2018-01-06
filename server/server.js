f28be325497bb757c58c596fb9efabb66fcf228e
Svetlana Jaricenco <jalana2805@gmail.com>
2018-01-06 00:01:38 -0500

    first commit

--- server/server.js
+++ server/server.js
@@ -4,60 +4,60 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(__dirname + '/../public' ));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 // DB
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book_db');
+ const mongoose = require('mongoose');
+ mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book_db');
+mongoose.connect('mongodb://localhost:27017/book_db');
+ const {Book} = require('./models/books');
+ const {Store} = require('./models/stores');
 
-const {Book} = require('./models/books');
-const {Store} = require('./models/stores');
-
-//POST
+ //POST
 app.post('/api/add/store',(req,res)=>{
 
-    const store = new Store({
-        name:req.body.name,
+     const store = new Store({
+         name:req.body.name,
         address:req.body.address,
-        phone:req.body.phone
+         phone:req.body.phone
     });
 
     store.save((err,doc)=>{
-        if(err) res.status(400).send(err);
+         if(err) res.status(400).send(err);
         res.status(200).send();
 
     })
-});
+ });
 
-app.post('/api/add/books',(req,res)=>{
+ app.post('/api/add/books',(req,res)=>{
 
     const book = new Book({
-        name: req.body.name,
-        author: req.body.author,
-        pages:  req.body.pages,
-        price:  req.body.price,
-        stores:  req.body.stores
-    });
+         name: req.body.name,
+         author: req.body.author,
+         pages:  req.body.pages,
+         price:  req.body.price,
+         stores:  req.body.stores
+     });
 
-    book.save((err,doc)=>{
-        if(err) res.status(400).send(err);
-        res.status(200).send();
-    })
+     book.save((err,doc)=>{
+         if(err) res.status(400).send(err);
+         res.status(200).send();
+     })
 
-})
+ })
 
 
-// GET
-app.get('/api/stores',(req,res)=>{
+ // GET
+ app.get('/api/stores',(req,res)=>{
 
     Store.find((err,doc)=>{
         if(err) res.status(400).send(err);
         res.send(doc)
-    })
+     })
 
-})
+ })
 
 
 app.get('/api/books',(req,res)=>{
